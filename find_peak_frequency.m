ba036ed73771f3cedac916c538d63a83c5514dd2
Qingyang Qiu <catherine.qiuqy@gmail.com>
2025-03-14 20:34:17 +0100

    Delete find_peak_frequency.m

--- find_peak_frequency.m
+++
@@ -1,9 +0,0 @@
-function peak_freq = find_peak_frequency(F, PSD, band)
-    % Select frequency range within specified band
-    valid_F = F(F >= band(1) & F <= band(2));
-    valid_PSD = PSD(F >= band(1) & F <= band(2));
-    
-    % Find peak frequency
-    [~, idx] = max(valid_PSD);
-    peak_freq = valid_F(idx);
-end
